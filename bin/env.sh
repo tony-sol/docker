eb6197009856d3adaf0c1a16d7e45ae08637f49f
Tony-Sol <tony.kent.nar.earth@gmail.com>
2023-05-05 04:20:56 +0300

    Update scripts

--- bin/env.sh
+++ bin/env.sh
@@ -3,6 +3,4 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd $(dirname ${SCRIPT_DIR}/../..)
 
-source "${SCRIPT_DIR}/../.env"
-
-COMPOSE="docker-compose --project-name ${COMPOSE_PROJECT_NAME} --file docker-compose.yml"
+COMPOSE="docker compose --file docker-compose.yml --env-file .env"
